13eacf39745d740be347d4effcae487e7429a0dc
Lee Fox <leefox78@live.com>
2021-05-21 22:09:21 -0500

    Mongoose Part 3

--- index.js
+++ index.js
@@ -4,23 +4,44 @@ const Campsite = require('./models/campsite');
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
+  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 connect.then(() => {
-  console.log('Connected correctly to server.');
+  console.log('Connected correctly to server');
 
   Campsite.create({
     name: 'React Lake Campground',
-    description: 'Test',
+    description: 'test',
   })
     .then(campsite => {
       console.log(campsite);
-      return Campsite.find();
+
+      return Campsite.findByIdAndUpdate(
+        campsite._id,
+        {
+          $set: { description: 'Updated Test Document' },
+        },
+        {
+          new: true,
+        }
+      );
     })
-    .then(campsites => {
-      console.log(campsites);
+    .then(campsite => {
+      console.log(campsite);
+
+      campsite.comments.push({
+        rating: 5,
+        text: 'What a magnificent view!',
+        author: 'Tinus Lorvaldes',
+      });
+
+      return campsite.save();
+    })
+    .then(campsite => {
+      console.log(campsite);
       return Campsite.deleteMany();
     })
     .then(() => {
@@ -28,6 +49,6 @@ connect.then(() => {
     })
     .catch(err => {
       console.log(err);
-      mongoose.connections.close();
+      mongoose.connection.close();
     });
 });
